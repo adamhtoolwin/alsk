e8c84bbef3264e880a0cb80c8b1b577fef8d7d81
Nopp <nopphonyel@gmail.com>
2020-04-12 12:02:34 +0700

    Splitting the data, plot val loss and stuff

--- alsk_proj.py
+++ alsk_proj.py
@@ -10,26 +10,37 @@ from tqdm import tqdm
 DATA_SET_PATH = 'dataset/data_preprocessed_python/'
 
 deap_dataset = DEAP_DATASET(DATA_SET_PATH)
+deap_dataset.set_participant_id(0)
+deap_loader = DataLoader(deap_dataset, shuffle=True, batch_size=1)
+
+deap_dataset_test = DEAP_DATASET(DATA_SET_PATH)
+deap_dataset_test.set_participant_id(1)
+deap_test_loader = DataLoader(deap_dataset, shuffle=True, batch_size=1)
 
 # MODEL_CONFIG
 INPUT_SIZE = 40
 HIDDEN_SIZE = 160
 OUTPUT_SIZE = 4
 model = SIMPLE_RNN(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
-deap_dataset.set_participant_id(0)
-deap_loader = DataLoader(deap_dataset, shuffle=True, batch_size=1)
 
 # TRAINING_CONFIG
 CRITERION = torch.nn.MSELoss()
 LR = 1e-5
 EPCH = 100
 optim = optim.Adam(model.parameters(), lr=LR)
+EXPORT_PATH = 'models/saved_weights/simple_rnn.pth'
 
 print("Starting training...")
 loss_hist = []
+val_loss_hist = []
 for i in tqdm(range(EPCH)):
     avg_loss = train(model, optim, CRITERION, deap_loader)
     loss_hist.append(avg_loss)
-
-plt.plot(loss_hist)
-plt.show()
+    val_loss = eval(model, CRITERION, deap_test_loader, eval_size=5)
+    export_or_not(val_loss, val_loss_hist, model, EXPORT_PATH)
+    val_loss_hist.append(val_loss)
+    if i % 1 == 0:
+        plt.plot(loss_hist, label="Training loss")
+        plt.plot(val_loss_hist, label="Validation loss")
+        plt.legend()
+        plt.show()
--- util/train.py
+++ util/train.py
@@ -1,11 +1,36 @@
 from numpy import mean
+import torch
+
+DBG = False
+
+
+def eval(model, criterion, data_loader, eval_size):
+    hidden = model.initHidden()
+    loss_hist = []
+    model.eval()
+    for i, (signal, label) in enumerate(data_loader):
+        output = None
+        if i > eval_size:
+            break
+        for j in range(signal.shape[2]):
+            pts_signal = signal[:, :, j]
+            output, hidden = model(pts_signal, hidden)
+        # print(output)
+        # print(output, label)
+        loss = criterion(output, label)
+        loss_hist.append(loss.item())
+    return mean(loss_hist)
 
 
 def train(model, optim, criterion, data_loader):
     hidden = model.initHidden()
     loss_hist = []
+    model.train()
     for i, (signal, label) in enumerate(data_loader):
         output = None
+        # Just add the option to skip the training if its too long
+        if DBG and i > 5:
+            break
         optim.zero_grad()
         for j in range(signal.shape[2]):
             pts_signal = signal[:, :, j]
@@ -18,3 +43,10 @@ def train(model, optim, criterion, data_loader):
         optim.step()
         loss_hist.append(loss.item())
     return mean(loss_hist)
+
+
+def export_or_not(val_loss, val_loss_hist, model, path):
+    if len(val_loss_hist) == 0:
+        torch.save(model.state_dict(), path)
+    elif val_loss < val_loss_hist[len(val_loss_hist) - 1]:
+        torch.save(model.state_dict(), path)
