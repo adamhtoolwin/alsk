68142e2ca98f46847eff818eb935f8553c342acd
AdamHtooLwin <adamhtoolwin@gmail.com>
2020-04-16 22:11:38 +0700

    Add one lstm layer and dropout

---
+++ alsk_lstm_v2.py
@@ -0,0 +1,66 @@
+from dataset.DEAP_DATASET import DEAP_DATASET, CombinedDeapDataset, ModularDeapDataset
+from models.simple_rnn import SIMPLE_RNN
+from models.lstm import EEGLSTM, EEGLSTM_V2
+from util.train import *
+from torch.utils.data import DataLoader
+import torch
+from torch import optim
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+DATA_SET_PATH = 'dataset/'
+
+CUDA = True
+gpu_id = '1'
+batch_size = 128
+device = torch.device("cuda:" + gpu_id if CUDA and torch.cuda.is_available() else "cpu")
+print("[SYS] Using", device)
+print("")
+
+deap_train_dataset = ModularDeapDataset(DATA_SET_PATH, train=True)
+deap_test_dataset = ModularDeapDataset(DATA_SET_PATH, train=False)
+
+# import pdb; pdb.set_trace()
+
+deap_train_loader = DataLoader(deap_train_dataset, shuffle=True, batch_size=batch_size)
+deap_test_loader = DataLoader(deap_test_dataset, shuffle=True, batch_size=batch_size)
+
+# MODEL_CONFIG
+INPUT_SIZE = 40
+HIDDEN_SIZE1 = 64
+HIDDEN_SIZE2 = 32
+HIDDEN_SIZE3 = 32
+OUTPUT_SIZE = 4
+EXPORT_PATH = 'models/saved_weights/lstm_v3_dropout.pth'
+
+model = EEGLSTM_V2(HIDDEN_SIZE1, HIDDEN_SIZE2, HIDDEN_SIZE3, batch_size)
+model.to(device)
+
+# TRAINING_CONFIG
+CRITERION = torch.nn.MSELoss()
+LR = 1e-4
+EPCH = 6000
+optim = optim.Adam(model.parameters(), lr=LR)
+
+# TRAINING VISUALIZE CONFIG
+PLOT_EVERY = 5
+
+print("==============================")
+print("Starting training...")
+loss_hist = []
+val_loss_hist = []
+for i in tqdm(range(EPCH)):
+    avg_loss = train_lstm(model, optim, CRITERION, deap_train_loader, device)
+    loss_hist.append(avg_loss)
+    val_loss = eval_lstm(model, CRITERION, deap_test_loader, device, eval_size=99999)
+    if not DBG:
+        export_or_not(val_loss, val_loss_hist, model, EXPORT_PATH)
+    val_loss_hist.append(val_loss)
+    # print(val_loss - avg_loss)
+    if i % PLOT_EVERY == 0 or i == EPCH-1:
+        plt.plot(loss_hist, label="Training loss")
+        plt.plot(val_loss_hist, label="Validation loss")
+        plt.legend()
+        plt.savefig("loss.png")
+        plt.show()
--- models/lstm.py
+++ models/lstm.py
@@ -3,17 +3,18 @@ import torch.nn as nn
 
 
 class EEGLSTM_V2(nn.Module):
-    def __init__(self, hidden_size1, hidden_size2, batch_size):
+    def __init__(self, hidden_size1, hidden_size2, hidden_size3, batch_size):
         super(EEGLSTM_V2, self).__init__()
 
         self.hidden_size1 = hidden_size1  # 64
         self.hidden_size2 = hidden_size2  # 32
+        self.hidden_size3 = hidden_size3  # 32
         self.batch_size = batch_size
 
         self.lstm1 = nn.LSTM(40, self.hidden_size1, batch_first=True)
-        # Put dropout here later :D
         self.dropout = nn.Dropout(p=0.2)
         self.lstm2 = nn.LSTM(self.hidden_size1, self.hidden_size2, batch_first=True)
+        self.lstm3 = nn.LSTM(self.hidden_size2, self.hidden_size3, batch_first=True)
 
         self.relu = nn.ReLU()
 
@@ -28,9 +29,15 @@ class EEGLSTM_V2(nn.Module):
 
         # Input Size (1, 8064, 40), Hidden/Cell Size (1, 1, hidden_size1) (1 eeg sequence for 1 video)
         x, _ = self.lstm1(x, (hidden[0], hidden[1]))  # Mem_inc [1st: 500MB]
+        x = self.dropout(x)
 
         # (1, 8064, hidden_size1), (1, 1, hidden_size2)
         x, _ = self.lstm2(x, (hidden[2], hidden[3]))  # Mem_inc [1st: 200MB, 2nd: 200MB]
+        x = self.dropout(x)
+
+        #
+        x, _ = self.lstm3(x, (hidden[4], hidden[5]))
+        x = self.dropout(x)
 
         x = self.fc(x)
         output = self.relu(x)
@@ -39,7 +46,8 @@ class EEGLSTM_V2(nn.Module):
 
     def initHidden(self):
         return [torch.zeros(1, self.batch_size, self.hidden_size1), torch.zeros(1, self.batch_size, self.hidden_size1),
-                torch.zeros(1, self.batch_size, self.hidden_size2), torch.zeros(1, self.batch_size, self.hidden_size2)]
+                torch.zeros(1, self.batch_size, self.hidden_size2), torch.zeros(1, self.batch_size, self.hidden_size2),
+                torch.zeros(1, self.batch_size, self.hidden_size3), torch.zeros(1, self.batch_size, self.hidden_size3)]
 
 
 class EEGLSTM(nn.Module):
