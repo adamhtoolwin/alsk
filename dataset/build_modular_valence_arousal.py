db8db26dd5b04cfaa85a104b60bfb899ae1e8315
AdamHtooLwin <adamhtoolwin@gmail.com>
2020-05-04 18:56:50 +0700

    Add for dataset build for val and arousal only

---
+++ dataset/build_modular_valence_arousal.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 16:46:41 2020
+
+@author: adam
+
+Unpickles all files and combines into one shuffled array.
+
+Builds one .dat file for each video file consisting of dictionary with keys:
+    1. data
+    2. labels
+"""
+
+import glob
+import pickle
+import numpy as np
+import os
+
+
+files = glob.glob("./data_preprocessed_python/*.dat")
+
+eegs = []
+labels = []
+
+for each_path in files:
+    dat = pickle.load(open(each_path, 'rb'), encoding='iso-8859-1')
+    eegs.append(dat['data'])
+    labels.append(dat['labels'])
+
+eegs_combined = np.asarray(eegs).reshape((32 * 40, 40, 8064))
+labels_combined = np.asarray(labels).reshape((32 * 40, 4))
+
+labels_reduced = labels_combined[:, 0:2]
+
+random_indexes = np.random.permutation(32 * 40)
+eegs_combined = eegs_combined[random_indexes]
+labels_reduced = labels_reduced[random_indexes]
+
+if not os.path.isdir('modular'):
+    os.makedirs('modular')
+
+for idx, pair in enumerate(zip(eegs_combined, labels_reduced)):
+    data = {
+        'data': pair[0],
+        'label': pair[1]
+    }
+    
+    with open('./modular/' + str(idx) + '.dat', 'wb') as f:
+        pickle.dump(data, f)
