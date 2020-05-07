8e8eb881a6bfcdd2c35112acbb70b79122ce4243
Adam <adamhtoolwin@gmail.com>
2020-05-07 22:14:49 +0700

    Port cross val code to TCNs

--- alsk_TCN_smallKern.py
+++ alsk_TCN_smallKern.py
@@ -1,5 +1,5 @@
-PARTICIPANT_NUM = 3  # This is constant... this must not be changed
-CROSS_VAL = 5  # This is constant... this must not be changed
+PARTICIPANT_NUM = 2  # This is constant... this must not be changed
+CROSS_VAL = 2  # This is constant... this must not be changed
 
 from dataset.DEAP_DATASET import DEAP_DATASET
 from tqdm.auto import trange
@@ -7,42 +7,43 @@ from torch.utils.data import DataLoader
 import torch.optim as optimizer
 import matplotlib.pyplot as plt
 
-from models.lstm import EEGLSTM
+from models.tcn import *
 from util.train import *
 import numpy as np
 import os
 
 # Initialize CUDA Device
 CUDA = True
-gpu_id = '0'
+gpu_id = '1'
 batch_size = 8
 device = torch.device("cuda:" + gpu_id if CUDA and torch.cuda.is_available() else "cpu")
 print("[SYS] Using", device)
 print("")
 
 # MODEL_CONFIG
+CHAN_LIST = [32, 24, 16, 10, 6, 2]  # The list of each convolutional layers
+KERN_SIZE = 5
+DROP_OUT = 0.2
 INPUT_SIZE = 32
-HIDDEN_SIZE1 = 64
-HIDDEN_SIZE2 = 32
-OUTPUT_SIZE = 4
-model = EEGLSTM(INPUT_SIZE, HIDDEN_SIZE1, HIDDEN_SIZE2, batch_size)
+model = EEG_TCN(CHAN_LIST, KERN_SIZE, DROP_OUT)
 # model.load_state_dict(torch.load(EXPORT_PATH, map_location=device))
 model.to(device)
 
 # PATH initialize
-EXPORT_PATH_DIR = 'models/saved_weights/lstm/Alhagry_variant/'
+EXPORT_PATH_DIR = 'models/saved_weights/tcn/smallKernel/'
 mkdir(EXPORT_PATH_DIR)
 
 # TRAINING_CONFIG
 CRITERION = torch.nn.MSELoss()
 LR = 1e-4
-EPCH = 1000
+EPCH = 10
 optim = optimizer.Adam(model.parameters(), lr=LR)
 
 print("===========[INFO REPORT]===========")
-print("Arch. [%d -> %d]" % (HIDDEN_SIZE1, HIDDEN_SIZE2))
 print("<I> Using model config")
-print("\tInput size :", INPUT_SIZE)
+print("\tChannels size :", CHAN_LIST)
+print("\tKernel size: ", KERN_SIZE)
+print("\tDropout: ", DROP_OUT)
 print("\tExport path :", EXPORT_PATH_DIR)
 print("<I> Using training config")
 print("\tBatch size :", batch_size)
@@ -53,12 +54,12 @@ print("\tOptimizer :", "Adam")
 print("Please check config...")
 input("\tPress ENTER to proceed.")
 
-print("Starting training GRU model...")
+print("Starting training TCN small kernel model...")
 
 # TRAINING VISUALIZE CONFIG
 PLOT_EVERY = 10
 
-DATA_SET_PATH = "../dataset"
+DATA_SET_PATH = "./dataset"
 train_dataset = DEAP_DATASET(DATA_SET_PATH, train=True, part_id=1, cross_val_id=1)
 test_dataset = DEAP_DATASET(DATA_SET_PATH, train=False, part_id=1, cross_val_id=1)
 
@@ -82,29 +83,31 @@ for p in range(1, PARTICIPANT_NUM + 1):
 
         loss_hist = []
         val_loss_hist = []
+        EXPORT_PATH_FILE = ""
         for i in trange(EPCH, desc="Epoch"):
-            avg_loss = train_lstm_gru(model, optim, CRITERION, deap_train_loader, device)
+            avg_loss = train_tcn(model, optim, CRITERION, deap_train_loader, device)
             loss_hist.append(avg_loss)
-            val_loss = eval_lstm_gru(model, CRITERION, deap_test_loader, device, eval_size=99999)
+            val_loss = eval_tcn(model, CRITERION, deap_test_loader, device, eval_size=99999)
             if not DBG:
-                EXPORT_PATH = EXPORT_PATH + "c" + str(c) + ".pth"
-                export_or_not(val_loss, val_loss_hist, model, EXPORT_PATH)
+                EXPORT_PATH_FILE = EXPORT_PATH + "c" + str(c) + ".pth"
+                export_or_not(val_loss, val_loss_hist, model, EXPORT_PATH_FILE)
             val_loss_hist.append(val_loss)
             # print(val_loss - avg_loss)
             if i % PLOT_EVERY == 0 or i == EPCH - 1:
+                plt.clf()
                 plt.plot(loss_hist, label="Training loss")
                 plt.plot(val_loss_hist, label="Validation loss")
-                plt.title("On participant" + str(p) + "cross id" + str(c))
+                plt.title("On participant " + str(p) + "cross id " + str(c))
                 plt.legend()
-                plt.savefig("loss_" + str(p) + "_" + str(c) + ".png")
+                plt.savefig(EXPORT_PATH + "loss_" + str(p) + "_" + str(c) + ".png")
                 plt.show()
 
         # After finish training, load the best model
-        print(">> Loading previous model from : " + EXPORT_PATH)
-        model.load_state_dict(torch.load(EXPORT_PATH, map_location=device))
+        print(">> Loading previous model from : " + EXPORT_PATH_FILE)
+        model.load_state_dict(torch.load(EXPORT_PATH_FILE, map_location=device))
         model.to(device)
-        train_loss = eval_lstm_gru(model, CRITERION, deap_train_loader, device, eval_size=99999)
-        val_loss = eval_lstm_gru(model, CRITERION, deap_test_loader, device, eval_size=99999)
+        train_loss = eval_tcn(model, CRITERION, deap_train_loader, device, eval_size=99999)
+        val_loss = eval_tcn(model, CRITERION, deap_test_loader, device, eval_size=99999)
 
         train_MSE_Loss_buffer.append(train_loss)
         test_MSE_Loss_buffer.append(val_loss)
